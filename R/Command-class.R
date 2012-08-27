60b82aa2a807de3358d77587e189e63c6521eb5a
Tengfei Yin <yintengfei@gmail.com>
2012-08-27 00:50:02 -0500

    remove widget, active, etc from command implementation

--- R/Command-class.R
+++ R/Command-class.R
@@ -2,21 +2,20 @@
 
 setClass("Command", representation(active = "logical"), prototype(active = TRUE))
 
-
 ### possibly supported methods
 ## rev(x)
-setGeneric("active", function(object, ...) standardGeneric("active"))
-setMethod("active", "Command", function(object) object@active)
+## setGeneric("active", function(object, ...) standardGeneric("active"))
+## setMethod("active", "Command", function(object) object@active)
 
-setGeneric("active<-", function(object, ... ,value) standardGeneric("active<-"))
-setReplaceMethod("active", "Command", function(object, value){
-  object@active <- value
-  object
-})
+## setGeneric("active<-", function(object, ... ,value) standardGeneric("active<-"))
+## setReplaceMethod("active", "Command", function(object, value){
+##   object@active <- value
+##   object
+## })
 ## active(), active<-()
 
 ## returns a widget for controlling and viewing this object
-setGeneric("widget", function(object, ...) standardGeneric("widget"))
+## setGeneric("widget", function(object, ...) standardGeneric("widget"))
 
 ## returns a widget containing an interactive visualization of the
 ## specified data in the context of the specified protocol
